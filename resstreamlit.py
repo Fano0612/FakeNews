31ace4d25e706e561c192555224e2f1d005224ab
Fano0612 <Fanomulyadi@gmail.com>
2022-12-14 17:25:35 +0700

    Progress

--- resstreamlit.py
+++ resstreamlit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
