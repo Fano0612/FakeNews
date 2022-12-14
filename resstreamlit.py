2d68a2e6907f2eb6e26fbda9d82d15f7cd39b45e
Fano0612 <Fanomulyadi@gmail.com>
2022-12-14 18:13:23 +0700

    Progress

--- resstreamlit.py
+++ resstreamlit.py
@@ -57,8 +57,7 @@ train_df['Statement'][2188]
 st.subheader('Stopwords')
 lemmatizer = WordNetLemmatizer()
 stpwrds = list(stopwords.words('english'))
-for i in stpwrds:
-    st.markdown("- " + I)
+st.markdown(stpwrds)
 
 st.subheader('Training')
 X_train, X_test, Y_train, Y_test = train_test_split(train_df['Statement'], label_train, test_size=0.3, random_state=1)
