132d29b6f7665cfbc8d870c3cc41c33a71e1b22b
Fano0612 <Fanomulyadi@gmail.com>
2022-12-14 17:26:39 +0700

    Progress

--- resstreamlit.py
+++ resstreamlit.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.metrics import confusion_matrix
 from matplotlib import pyplot as plt
 from sklearn.linear_model import PassiveAggressiveClassifier
 from nltk.stem import WordNetLemmatizer
